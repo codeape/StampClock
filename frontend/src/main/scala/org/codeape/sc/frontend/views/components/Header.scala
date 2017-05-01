0e6bb1ffba13fed09d2bcf2246296f0c94cbb113
Oscar Norlander <oscar-no@codeape.org>
2017-05-01 21:46:00 +0200

    Trying out bootstrap
     * Changed brand in navbar

--- frontend/src/main/scala/org/codeape/sc/frontend/views/components/Header.scala
+++ frontend/src/main/scala/org/codeape/sc/frontend/views/components/Header.scala
@@ -1,16 +1,16 @@
 package org.codeape.sc.frontend.views.components
 
+import io.udash.bootstrap.BootstrapStyles
 import io.udash.bootstrap.navs.{UdashNav, UdashNavbar}
 import io.udash.properties.seq.SeqProperty
 import io.udash.properties.single.CastableProperty
 import org.codeape.sc.frontend.Context._
-import org.codeape.sc.frontend.IndexState
 import org.scalajs.dom.raw.Element
 
 import scalatags.JsDom.all._
 
 object Header {
-  private val brand = a(href := IndexState.url)().render
+  private val brand = div(BootstrapStyles.Navigation.navbarBrand)("StampClock").render
 
   case class NavItem(href: String, imageSrc: String, name: String)
   private val navItems: SeqProperty[Header.NavItem, CastableProperty[Header.NavItem]] = SeqProperty.empty[Header.NavItem]
