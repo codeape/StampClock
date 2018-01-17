8d7b0d4dfbe8b9aa5d6b6275d294fac67fe31ef9
Oscar Norlander <oscar-no@codeape.org>
2018-01-17 22:08:38 +0100

    Removed AuthFilter directives
     * Removed unnecessary method directives.

--- backend/src/main/scala/org/codeape/sc/backend/filters/AuthFilter.scala
+++ backend/src/main/scala/org/codeape/sc/backend/filters/AuthFilter.scala
@@ -3,7 +3,7 @@ package org.codeape.sc.backend.filters
 import akka.actor.ActorSystem
 import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.model.StatusCodes.Unauthorized
-import akka.http.scaladsl.server.{Directive, Route}
+import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
 
 class AuthFilter(system: ActorSystem) {
@@ -11,10 +11,8 @@ class AuthFilter(system: ActorSystem) {
   val log: LoggingAdapter = Logging.getLogger(system, this)
   log.info("Created Auth filter")
 
-  val allMethods: Directive[Unit] = delete | get | head | options | patch | post | put
-
   def route: Route = pathPrefix("") {
-    (allMethods & optionalHeaderValueByName("stamptoken")) { headerVal =>
+    optionalHeaderValueByName("stamptoken") { headerVal =>
       log.debug(s"Received token \n")
       headerVal match {
         case Some(token) => if (token == "pw") reject else complete(Unauthorized)
